d5be2d6f5d3c002fd9c1edb31443b436b1aab558
k2hyun4 <k2hyun4@hoyun.co.kr>
2020-04-24 12:33:36 +0900

    feat : test222

--- app.py
+++ app.py
@@ -5,8 +5,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World222!'
 
+
+@app.route('/test')
+def test():
+    return test2(ll=1, l222=3)
+
+
+def test(values: dict):
+    for key in values.keys():
+        print(key)
+        print(values[key])
+
+    return 'complete'
+
+
+def test2(**kwargs):
+    return test(kwargs)
+
+
+values = {
+    'a': 123
+}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80')
+    app.run(debug=True, host='0.0.0.0', port='8080')
